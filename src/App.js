990009905de4f11a697ef195844968abb50ae09d
jiyun <sprare06@gmail.com>
2022-08-06 11:17:30 +0900

    app.js에 있던 기존 callFlickr함수를 recoil의 selectorFamily를 이용해서 순서함수 형태로 api.js에 등록

--- src/App.js
+++ src/App.js
@@ -11,16 +11,6 @@ function App() {
   const [_, setPic] = useRecoilState(flickrState);
   const tag = useRecoilValue(tagState);
 
-  const callFlickr = async () => {
-    const key = "4f2ed95542fa600d1ed1488dd32b341b";
-    const method_search = "flickr.photos.search";
-    const url = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${key}&per_page=10&nojsoncallback=1&format=json&tags=${tag}`;
-
-    const res = await fetch(url);
-    const body = await res.json();
-    setPic(body.photos.photo);
-  };
-
   useEffect(() => {
     callFlickr();
   }, [tag]);
